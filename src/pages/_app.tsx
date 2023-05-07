2e6603fdf88e8304750b154edc2153635c998c29
an678-mhg <an567008@gmail.com>
2023-05-07 23:22:44 +0700

    fix bug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,12 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 import "@/src/styles/globals.css";
 import type { AppProps } from "next/app";
 import NextNProgress from "nextjs-progressbar";
-import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
       <NextNProgress color="#CAE962" options={{ showSpinner: false }} />
       <Component {...pageProps} />
-      <Toaster />
     </div>
   );
 }
