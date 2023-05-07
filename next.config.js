e62ed4c3b2b2964c32f851dd4c5dd8bff37ca0cb
an678-mhg <an567008@gmail.com>
2023-05-07 20:59:34 +0700

    add timeout to generate static page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  staticPageGenerationTimeout: 1000,
 };
 
 module.exports = nextConfig;
--- src/utils/client.ts
+++ src/utils/client.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const client = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  timeout: 10000,
 });
 
 export default client;
