5b86720865e73b69d5a35e12d256665519a7de73
an678-mhg <an567008@gmail.com>
2023-05-17 22:00:42 +0700

    fix bug note component

--- src/components/Watch/Note.tsx
+++ src/components/Watch/Note.tsx
@@ -6,8 +6,9 @@ const Note = () => {
     <div className="mt-5">
       <TitlePrimary title="Note" />
       <p className="text-[14px] font-normal mt-4">
-        If you can't see the movie, try f5 again, change the source, or use an
-        iframe
+        {
+          "If you can't see the movie, try f5 again, change the source, or use an iframe"
+        }
       </p>
     </div>
   );
